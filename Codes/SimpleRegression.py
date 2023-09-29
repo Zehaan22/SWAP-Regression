672f8482f345d8e73f22c95124c6995237fbaf64
Zehaan <zehaan22@iitk.ac.in>
2023-09-30 01:17:43 +0530

    Initial Commit

---
+++ Codes/SimpleRegression.py
@@ -0,0 +1,21 @@
+"""File to write the code for simple linear regression."""
+
+
+# Code to generate parameters for simple linear regression
+def generate_parameters(X, Y):
+    Sx = 0  # Sig(xi)
+    Sy = 0  # Sig(yi)
+    Sxy = 0  # Sig[(xi)*(yi)]
+    Sxx = 0  # Sig(xi^2)
+    n = len(X)
+    for i in range(n):
+        Sx += X[i]
+        Sy += Y[i]
+        Sxy += X[i] * Y[i]
+        Sxx += X[i] * X[i]
+
+    # Standard Formula for linear regression
+    b1 = (Sxy - ((Sy)*(Sx))/n)/(Sxx - (Sx**2)/n)
+    b0 = Sy/n - b1*(Sx/n)
+
+    return (b0, b1)
